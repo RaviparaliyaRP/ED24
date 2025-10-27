d72255492a59c4ebe76f602e9cd83156d536bfe7
PARALIYA RAVI <psquaregst@gmail.com>
2025-10-27 14:59:39 +0530

    Add top padding to hero content so it's visible below navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export default function Hero() {
       <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-transparent" />
 
       {/* Content */}
-      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center pt-32">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
